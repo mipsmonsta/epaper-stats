64892fe1ac52b3d5473a54de8cacc85a65f043c7
mipsmonsta <shibainusoft@gmail.com>
2024-01-28 13:01:17 +0800

    First commit

---
+++ examples/estats.py
@@ -0,0 +1,105 @@
+import sys
+import os
+picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
+libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib/waveshare_epd')
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
+import logging
+import epd2in7
+import time
+from PIL import Image,ImageDraw,ImageFont, ImageOps
+import utility
+import traceback
+
+def drawForegroundStats(baseImage: Image, jitter:int = 0) -> Image:
+
+    # ready font
+    defaultFont = 'Roboto-Bold.ttf'
+    font35 = ImageFont.truetype(os.path.join(picdir, defaultFont), 35)
+    font20 = ImageFont.truetype(os.path.join(picdir, defaultFont), 20)
+    font15 = ImageFont.truetype(os.path.join(picdir, defaultFont), 15)
+    font10 = ImageFont.truetype(os.path.join(picdir, defaultFont), 10)
+
+    # draw object
+    draw = ImageDraw.Draw(baseImage)
+
+    xCoord = 50 + jitter
+
+    # hostname stats
+    hostNameText = utility.SysUtils.get_hostname()
+    hostIPText = utility.SysUtils.get_ip()
+    draw.text((xCoord, 15), hostNameText, font=font20, fill=255)
+    draw.text((xCoord, 40), hostIPText, font=font15, fill=255)
+
+    # temperature
+    tempText = utility.SysUtils.get_temp()
+    draw.text((xCoord, 90), tempText, font=font20, fill=0)
+
+    # mem stats: used, total and utilised
+    memStats = utility.SysUtils.get_mem_tuple()
+    draw.text((xCoord, 145), memStats[0], font=font15, fill=255)
+    draw.text((xCoord, 165), memStats[1], font=font10, fill=255)
+    draw.text((xCoord, 185), memStats[2], font=font15, fill=255)
+
+    # datetime stats snapshot
+    dateTimeStats = utility.SysUtils.get_datetime()
+    dateTimeStats = dateTimeStats.split(' ')
+    draw.text((xCoord, 215), dateTimeStats[0], font=font10, fill=0)
+    draw.text((xCoord, 235), dateTimeStats[1], font=font15, fill=0)
+
+    return baseImage
+
+def main(): 
+
+    logging.basicConfig(level=logging.INFO)
+
+    try:
+
+        logging.info("epd2in7 Demo")   
+        epd = epd2in7.EPD()
+        isJitter = False
+        while True:
+
+            logging.info("init and Clear")
+            epd.init()
+            epd.Clear(0xFF)
+
+            '''4Gray display'''
+            logging.info("4Gray display--------------------------------")
+            # epd.Init_4Gray()
+            # estats background
+            filePath = os.path.join(picdir, 'e_paper_stats.jpg')
+            Himage = Image.open(filePath)
+            # Process to greyscale (4 color) 
+            GImage = Himage.convert('L')
+            GImage.quantize(colors=4)
+
+            # draw foreground stats
+            GImage = drawForegroundStats(GImage, jitter= 5 if isJitter else 0)
+
+            epd.display(epd.getbuffer(GImage))
+            # epd.display_4Gray(epd.getbuffer_4Gray(GImage))
+
+            # toggle jitter
+            isJitter = not isJitter
+
+            # logging.info("Clear...")
+            # epd.Clear(0xFF)
+            logging.info("Goto Sleep...")
+            epd.sleep()
+            time.sleep(30)
+
+    except IOError as e:
+        traceback.print_exc()
+        logging.info(e)
+    
+    except KeyboardInterrupt:    
+        logging.info("ctrl + c:")
+        epd2in7.epdconfig.module_exit(cleanup=True)
+        exit()
+    finally:
+        epd.sleep()
+
+if __name__ == "__main__":
+    main()
---
+++ lib/waveshare_epd/utility.py
@@ -0,0 +1,161 @@
+from PIL import Image
+import os
+import pathlib
+import subprocess
+from datetime import datetime
+import logging
+import re
+
+class ImageUtils:
+    @staticmethod
+    def isPictureExtensionFile(filePath):
+        _, fileExt = os.path.splitext(filePath)
+        return fileExt.lower() == '.jpg'
+
+    @staticmethod
+    def autoRotateImage(image: Image) -> Image:
+
+        # Check for Exif orientation information
+        try:
+            exif = image._getexif()
+            if exif is not None:
+                orientation = exif.get(274)
+        
+                if orientation == 8 or 4:
+                    image = image.rotate(90, expand = True)
+                else:
+                    image = image.rotate(180, expand = True)
+
+        except (AttributeError, KeyError, IndexError):
+            # Ignore cases where Exif informaiton is not availble
+
+            pass
+
+        return image
+
+    
+    '''Resize image to be full cropped with no black border. 
+        Image width or Image height will match width or height respectively
+        if aspect ratio of image defers from the target aspect ratio'''
+    @staticmethod
+    def resizeImage(image: Image, width, height)-> Image:
+
+        image_width, image_height = image.size
+        aspect_ratio = image_width / image_height
+
+        target_aspect_ratio = width / height
+
+        # Determine to resize based on width or height
+
+        # image's width is longer than height, so we crop width
+        if aspect_ratio > target_aspect_ratio: 
+        
+            # resize based on target height and then crop width
+            new_width = int(height * aspect_ratio)
+            resizedImage = image.resize((new_width, height), Image.ANTIALIAS)
+            crop_x = (new_width - width) // 2
+            crop_y = 0
+
+
+        else:
+
+            new_height = int(width / aspect_ratio)
+            resizedImage = image.resize((width, new_height), Image.ANTIALIAS)
+            crop_x = 0
+            crop_y = (new_height - height) // 2
+
+        cropped_image = resizedImage.crop((crop_x, crop_y, crop_x + width, crop_y + height))
+    
+        return cropped_image
+
+
+class SysUtils:
+    logger = logging.getLogger('Utils')
+    current_dir = str(pathlib.Path(__file__).parent.parent.resolve())
+    temp_unit = 'C'
+
+    @staticmethod
+    def shell_cmd(cmd):
+        return subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT).decode("utf-8")
+
+    @staticmethod
+    def get_text_center(display, text, font):
+        w, h = SysUtils.get_text_size(display, text, font)
+        left = (display.width - w) / 2
+        top = (display.height - h) / 2
+        return [left, top]
+
+    @staticmethod
+    def get_text_size(display, text, font):
+        left, top, right, bottom = display.draw.textbbox((0, 0), text, font=font)
+        width = right - left if left > right else right - left
+        height = top - bottom if bottom < top else bottom - top
+        return [width, height]
+
+    @staticmethod
+    def requires_scroller(display, text, font):
+        w, unused = SysUtils.get_text_size(display, text, font)
+        return display.width < w
+
+    @staticmethod
+    def get_hostname(opt = ""):
+        return str(SysUtils.shell_cmd("hostname " + opt + "| cut -d\' \' -f1")).strip()
+
+    @staticmethod
+    def get_ip():
+        return SysUtils.get_hostname('-I')
+
+    @staticmethod
+    def get_datetime(format = None):
+        if not format:
+            format = SysUtils.datetime_format if hasattr(SysUtils, 'datetime_format') else "%d/%m/%Y %H:%M:%S"
+        return datetime.now().strftime(format)
+    
+    @staticmethod
+    def get_temp():
+        temp =  float(SysUtils.shell_cmd("cat /sys/class/thermal/thermal_zone0/temp")) / 1000.00
+
+        if (hasattr(SysUtils,'temp_unit') and SysUtils.temp_unit == 'F'):
+            temp = "%0.1f °F " % (temp * 9.0 / 5.0 + 32)
+        else:
+            temp = "%0.1f °C " % (temp)
+
+        return temp
+    
+    @staticmethod
+    def get_storage_tuple() -> tuple[str]:
+        storage =  SysUtils.shell_cmd('df -h | awk \'$NF=="/"{printf "%d,%d,%s", $3,$2,$5}\'')
+        storage = storage.split(',')
+        return (f"USED: {storage[0]} GB", 
+                f"TOTAL: {storage[1]} GB", 
+                f"UTILISED: {storage[2]}")
+    
+    @staticmethod
+    def get_mem_tuple() -> tuple[str]:
+        mem =  SysUtils.shell_cmd("free -m | awk 'NR==2{printf \"%.1f,%.1f,%.0f%%\", $3/1000,$2/1000,$3*100/$2 }'")
+        mem = mem.split(',')
+        return (f"USED: {mem[0]} GB", 
+                f"TOTAL: {mem[1]} GB", 
+                f"UTILISED: {mem[2]}")
+
+    @staticmethod
+    def compile_text(text, additional_replacements = {}):
+        replacements = {
+            "{hostname}": lambda prop: SysUtils.get_hostname(),
+            "{ip}": lambda prop: SysUtils.get_ip(),
+            "{datetime}": lambda prop: SysUtils.get_datetime()
+        }
+        replacements = {**replacements, **additional_replacements}
+        regex = re.compile("(%s)" % "|".join(map(re.escape, replacements.keys())))
+        return regex.sub(lambda match: replacements[match.string[match.start():match.end()]](match.string[match.start():match.end()]), text)
+
+    @staticmethod
+    def does_text_width_fit(display, text, font):
+        left, top, right, bottom = display.draw.textbbox((0, 0), text, font=font)
+        return display.width > right - left
+
+    @staticmethod
+    def slugify(text):
+        maxlength = 15
+        slug = re.sub(r'[^a-z0-9\_ ]+', '', text.lower().strip()).replace(" ", "_")[0:maxlength].strip('_')
+        return slug
